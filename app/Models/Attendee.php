18e9d351d91b80d04f5bd75cc48456e5765a505b
kkoouuya <koya.iwabuchi@rakus-partners.co.jp>
2021-12-23 14:20:11 +0900

    部署と日付でfiltering

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -11,12 +11,32 @@ class Attendee extends UuidModel
 {
     use HasFactory, SoftDeletes;
 
-    protected $dates = ['deleted_at'];
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
 
     protected $fillable = [
         'name',
         'dep_id',
         'number'
     ];
+
+    public function scopeDepartmentFilter($query, ?string $dep_id)
+    {
+        if (!is_null($dep_id)) {
+            return $query->where('dep_id', $dep_id);
+        }
+        return $query;
+    }
+
+    public function scopeCreatedFilter($query, ?string $date)
+    {
+        if (!is_null($date)) {
+            return $query->where('created_at', 'like', "%{$date}%");
+        }
+        return $query;
+    }
 
 }
